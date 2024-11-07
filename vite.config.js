11100107bc2dee66661b799b284bd52419e36de4
behhruz <bsatimbayev05@gmail.com>
2024-11-07 19:35:56 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://7869-89-236-218-10.ngrok-free.app', // URL для прокси
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // убираем префикс `/api` для совпадения с серверным маршрутом
+      }
+    }
+  }
 })
